2312cb37e72861aa1a301b41918cf66b9cbe24d3
irakli00 <iraklitsurtsumia@gmail.com>
2024-09-09 16:18:21 +0400

    lets test rotate

--- scripts/script.js
+++ scripts/script.js
@@ -9,18 +9,50 @@ let turn = 0;
 
 const putInLandscape = function(){
   if (!window.matchMedia("(orientation: landscape)").matches) {
-    page.innerHTML = htmls.notLandscape
+    console.log('before',[...page.children]);
+
+    [...page.children].forEach(el=>{
+      el.style.opacity = 0
+      el.style.height = 0
+    })
+
+  //!page.querySelector('.rotate-phone-bro') && page.insertAdjacentHTML('beforeend',htmls.notLandscape)
+  
+  page.insertAdjacentHTML('beforeend',htmls.notLandscape)
+
+  console.log('after',[...page.children]);
+
   }else{
     if(page.querySelector('.rotate-phone-bro')){
-      page.innerHTML= htmls.startingPage 
+
+      page.querySelector('.rotate-phone-bro').style.display = 'none';
+
+      page.removeChild(page.querySelector('.rotate-phone-bro'));
+
+      //page.innerHTML= htmls.startingPage;
+
+      console.log('else before',[...page.children]);
+
+      [...page.children].forEach(el=>{
+        el.style.opacity = 1
+        el.style.height = ''
+      })
+
+      console.log('else after',[...page.children]);
     }
   }
 }
 
 const startGame = function () {
   if (!window.matchMedia("(orientation: landscape)").matches) {
-    page.innerHTML = htmls.notLandscape
+    [...page.children].forEach(el=>{
+      el.style.opacity = 0
+      el.style.height = 0
+    })
+  
+    page.insertAdjacentHTML('beforeend', htmls.notLandscape)
   }
+    //if loaded in verticaly
 
   window.addEventListener('resize',()=>putInLandscape())
 
